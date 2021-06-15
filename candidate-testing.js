bb7af780db24b3214e5570562a4ad3ee1c9b485e
aaroncosmith <55358352+aaroncosmith@users.noreply.github.com>
2021-06-15 21:11:49 +0000

    part 2 finished

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,38 +8,40 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space?","True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  require("readline-sync");
   candidateName = input.question("Please type your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  require("readline-sync");
-  canidateAnswer = input.question(question);
+  for (let i = 0; i < 5; i++) {
+    canidateAnswer = input.question(questions[i])
+    candidateAnswers.push(canidateAnswer);
+  }
+  console.log(candidateAnswers);
 }
 
+
 function gradeQuiz(candidateAnswers) {
+  console.log("================================")
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  let grade;
-  if (canidateAnswer === correctAnswer){
-    grade = "A+";
-  } else {
-    grade = "F-";
+  for (let i = 0; i < 5; i++) {
+  console.log(`
+  Question:
+  *${questions[i]}
+  Your Answer:
+  *${candidateAnswers[i]}
+  Correct Answer:
+  *${correctAnswers[i]}
+  `)
   }
-  console.log("Your grade is: ", grade);
-
-  
-  
-
-  return grade;
 }
 
 function runProgram() {
